983d61fd7c9752a47d30bf27230d47d218a2009a
Erik W. Gren <ErikWDev@gmail.com>
2025-01-18 11:41:19 +0100

    metal: implement new api for depth/stencil and resolve settings

--- blade-graphics/src/lib.rs
+++ blade-graphics/src/lib.rs
@@ -1144,12 +1144,14 @@ pub struct RenderTargetSet<'a> {
     pub depth_stencil: Option<DepthStencilRenderTarget>,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum ResolveMode {
     /// Default for MSAA color resolve
     Average,
+
     /// Default for depth/stencil resolve
     Sample0,
+
     Min,
     Max,
 }
