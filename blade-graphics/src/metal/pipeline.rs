4d493384399edd7abb1c295520ba3bb9ef8fd992
Erik W. Gren <ErikWDev@gmail.com>
2026-01-06 08:51:18 +0100

    don't force loop bounding on metal

--- blade-graphics/src/metal/pipeline.rs
+++ blade-graphics/src/metal/pipeline.rs
@@ -277,13 +277,13 @@ impl super::Context {
             ),
             inline_samplers: Default::default(),
             spirv_cross_compatibility: false,
-            fake_missing_bindings: false,
+            fake_missing_bindings: true,
             per_entry_point_map: Some((sf.entry_point.to_string(), naga_resources))
                 .into_iter()
                 .collect(),
             bounds_check_policies: naga::proc::BoundsCheckPolicies::default(),
             zero_initialize_workgroup_memory: false,
-            force_loop_bounding: true,
+            force_loop_bounding: false,
         };
 
         let pipeline_options = msl::PipelineOptions {
